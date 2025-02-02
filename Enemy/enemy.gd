a33d6d440a19d83951c82a0edbc6765602a6d61b
Jonathan Carey <jonathan.carey1995@gmail.com>
2025-02-02 14:37:14 -0500

    added weapon, player health/damage and enemy health/damage

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -1,20 +1,28 @@
 extends CharacterBody3D
-
+class_name Enemy
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+
+@export var max_hitpoints := 100
+@export var attack_range := 1.5
+@export var attack_damage := 20
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-
 var player
 var provoked := false
 var aggro_range := 12.0
+var hitpoints: int = max_hitpoints:
+	set(value):
+		hitpoints = value
+		if hitpoints <= 0:
+			queue_free()
+		provoked = true
 
-@export var attack_range := 1.5
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
@@ -56,3 +64,4 @@ func look_at_target(direction: Vector3) -> void:
 	
 func attack() -> void:
 	print("enemy attack")
+	player.hitpoints -= attack_damage
