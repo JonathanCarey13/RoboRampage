6a69a7b3a3c804d58697faa20cf96b9876529c28
Jonathan Carey <jonathan.carey1995@gmail.com>
2025-02-04 17:25:50 -0500

    added gun particles and subviewports

--- Weapons/hitscan_weapon.gd
+++ Weapons/hitscan_weapon.gd
@@ -4,11 +4,12 @@ extends Node3D
 @export var recoil := 0.05
 @export var weapon_mesh: Node3D
 @export var weapon_damage := 15
+@export var muzzle_flash: GPUParticles3D
+@export var sparks: PackedScene
 
 @onready var cooldown_timer: Timer = $CooldownTimer
 @onready var weapon_position: Vector3 = weapon_mesh.position
 @onready var ray_cast_3d: RayCast3D = $RayCast3D
-
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,9 +21,13 @@ func _process(delta: float) -> void:
 	weapon_mesh.position = weapon_mesh.position.lerp(weapon_position, delta * 10.0)
 			
 func shoot() -> void:
+	muzzle_flash.restart()
 	cooldown_timer.start(1.0 / fire_rate)
 	var collider = ray_cast_3d.get_collider()
 	printt("Weapon fired!!", collider)
 	weapon_mesh.position.z += recoil
 	if collider is Enemy:
 		collider.hitpoints -= weapon_damage
+	var spark = sparks.instantiate()
+	add_child(spark)
+	spark.global_position = ray_cast_3d.get_collision_point()
